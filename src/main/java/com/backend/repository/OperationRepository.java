6d0d67a95e107e518f23a716131ca6d981196793
Leye-ELhadji12 <86428272+Leye-ELhadji12@users.noreply.github.com>
2024-05-12 13:04:05 +0200

    query

--- src/main/java/com/backend/repository/OperationRepository.java
+++ src/main/java/com/backend/repository/OperationRepository.java
@@ -14,4 +14,8 @@ public interface OperationRepository extends JpaRepository<Operation, Long> {
     List<Operation> findByAccountId(String accountId);
     Page<Operation> findByAccountId(String accountId, Pageable pageable);
 
+//    @Query("SELECT NEW com.backend.dto.OperationCustomerDTO( c.id, op.id, c.id, op.amount, op.description, op.operationdate, op.typeOp )" +
+//            "FROM Operation op, Account a, Customer c " +
+//            "WHERE c.id =: customerId AND op.account.id=a.id AND c.id=a.customer.id")
+//    Page<OperationCustomerDTO> getAllHistoriesByCustomerId(@Param("customerId") int customerId, Pageable pageable);
 }
